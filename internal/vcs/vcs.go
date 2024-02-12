2185d5a974a154af0d4cb20a9e16b1a327fb89a1
David_OH <erijesudo@gmail.com>
2024-02-12 22:35:21 +0100

    added vcs

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -6,12 +6,17 @@ import (
 )
 
 func Version() string {
-	var revision string
-	var modified bool
+	var (
+		time     string
+		revision string
+		modified bool
+	)
 	bi, ok := debug.ReadBuildInfo()
 	if ok {
 		for _, s := range bi.Settings {
 			switch s.Key {
+			case "vcs.time":
+				time = s.Value
 			case "vcs.revision":
 				revision = s.Value
 			case "vcs.modified":
@@ -22,7 +27,7 @@ func Version() string {
 		}
 	}
 	if modified {
-		return fmt.Sprintf("%s-dirty", revision)
+		return fmt.Sprintf("%s-%s-dirty", time, revision)
 	}
-	return revision
+	return fmt.Sprintf("%s-%s", time, revision)
 }
