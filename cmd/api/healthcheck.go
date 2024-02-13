2e3ef57e43f032581cd01976da72e3ff677848d2
David_OH <erijesudo@gmail.com>
2024-02-13 11:58:47 +0100

    Working Api

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,11 +5,7 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// data := map[string]string{
-	// 	"status":      "available",
-	// 	"environment": app.config.env,
-	// 	"version":     version,
-	// }
+
 	env := envelope{"status": "available",
 		"system_info": map[string]string{"environment": app.config.env,
 			"version": version}}
